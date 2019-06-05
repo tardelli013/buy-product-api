0adba7d4c4b3c6128d87c6b26ef28cd403ae46c7
tardelli.moura <tardelli.moura@iteris.com.br>
2019-06-05 16:08:53 -0300

    enable feign clients

--- src/main/java/br/com/tardelli/buyproduct/BuyProductApplication.java
+++ src/main/java/br/com/tardelli/buyproduct/BuyProductApplication.java
@@ -2,8 +2,10 @@ package br.com.tardelli.buyproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BuyProductApplication {
 
     public static void main(String[] args) {
