e3468dd58f6cc268d5aaee3954689fab425509e8
tardelli.moura <tardelli.moura@iteris.com.br>
2019-06-05 16:11:19 -0300

    test request consumer CentralBankBrazilClient

--- src/main/java/br/com/tardelli/buyproduct/rest/TestServiceRest.java
+++ src/main/java/br/com/tardelli/buyproduct/rest/TestServiceRest.java
@@ -1,5 +1,8 @@
 package br.com.tardelli.buyproduct.rest;
 
+import br.com.tardelli.buyproduct.consumer.CentralBankBrazilClient;
+import br.com.tardelli.buyproduct.model.SelicRate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +12,16 @@ import java.util.List;
 @RestController
 public class TestServiceRest {
 
+    @Autowired
+    private CentralBankBrazilClient centralBankBrazilClient;
+
     @GetMapping("/people")
-    public List<String> listPeople() {
-        return Arrays.asList("tardelli","aguiar","moura");
+    public List<SelicRate> listPeople() {
+
+        List<SelicRate> selicRates = centralBankBrazilClient.findSelicRates("json", "01/01/2010", "30/01/2010");
+        List<SelicRate> selicRatesByLastDays = centralBankBrazilClient.findSelicRatesByLastDays(3, "json");
+
+        return selicRatesByLastDays;
     }
 
 }
