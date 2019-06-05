fcf78659a04178b0c29ba29d2e296ad2b6e55519
tardelli.moura <tardelli.moura@iteris.com.br>
2019-06-05 16:10:12 -0300

    add feign client to central bank of brazil, add model SelicRate

---
+++ src/main/java/br/com/tardelli/buyproduct/consumer/CentralBankBrazilClient.java
@@ -0,0 +1,21 @@
+package br.com.tardelli.buyproduct.consumer;
+
+import br.com.tardelli.buyproduct.model.SelicRate;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "CentralBankBrazil", url = "https://api.bcb.gov.br/")
+public interface CentralBankBrazilClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "dados/serie/bcdata.sgs.11/dados")
+    List<SelicRate> findSelicRates(@RequestParam(name = "formato") String format, @RequestParam(name = "dataInicial") String initialDate, @RequestParam(name = "dataFinal") String finalDate);
+
+    @RequestMapping(method = RequestMethod.GET, value = "dados/serie/bcdata.sgs.11/dados/ultimos/{lastDays}")
+    List<SelicRate> findSelicRatesByLastDays(@PathVariable("lastDays") Integer lastDays, @RequestParam(name = "formato") String format);
+
+}
---
+++ src/main/java/br/com/tardelli/buyproduct/model/SelicRate.java
@@ -0,0 +1,11 @@
+package br.com.tardelli.buyproduct.model;
+
+import lombok.Data;
+
+@Data
+public class SelicRate {
+
+    private String data;
+    private String valor;
+
+}
