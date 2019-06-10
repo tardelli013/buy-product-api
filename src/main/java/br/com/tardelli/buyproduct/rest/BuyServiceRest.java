e792b8286c75575ad3e72b962d8f2bd1699cd07e
tardelli.moura <tardelli.moura@iteris.com.br>
2019-06-10 13:10:38 -0300

    Add Swagger API Doc

--- src/main/java/br/com/tardelli/buyproduct/rest/BuyServiceRest.java
+++ src/main/java/br/com/tardelli/buyproduct/rest/BuyServiceRest.java
@@ -3,6 +3,8 @@ package br.com.tardelli.buyproduct.rest;
 import br.com.tardelli.buyproduct.dto.BuyRequestDTO;
 import br.com.tardelli.buyproduct.dto.InstallmentDTO;
 import br.com.tardelli.buyproduct.service.BuyService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,6 +17,7 @@ import java.util.List;
 import java.util.Objects;
 
 @RestController
+@Api(value = "BuyProduct")
 public class BuyServiceRest {
 
     private BuyService buyService;
@@ -24,12 +27,22 @@ public class BuyServiceRest {
         this.buyService = buyService;
     }
 
+    @ApiOperation(
+            value = "Buy Product",
+            response = InstallmentDTO.class,
+            notes = "Return List Installments by Buy Request \n\n\n " +
+                    "<b> Required Parameters </b> \n\n " +
+                    "qtdeParcelas \n\n " +
+                    "valorEntrada \n\n " +
+                    "codigo \n\n" +
+                    "nome \n\n " +
+                    "valor \n\n ")
     @PostMapping(path = "/buy/", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<InstallmentDTO>> buy(@RequestBody BuyRequestDTO buyRequest) {
+    public ResponseEntity buy(@RequestBody BuyRequestDTO buyRequest) {
         List<InstallmentDTO> installments = buyService.buy(buyRequest);
 
         if (Objects.nonNull(installments)) {
-            return new ResponseEntity<>(installments, HttpStatus.OK);
+            return new ResponseEntity<>(installments, HttpStatus.CREATED);
         } else {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
